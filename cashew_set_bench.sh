a169e43b35838ea827c219f79daac32f4de827bb
Samee Zahur <sza4uq@virginia.edu>
2018-01-26 00:02:36 -0800

    Reverted back to using g++

--- cashew_set_bench.sh
+++ cashew_set_bench.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
-clang++ -O3 --std=c++11 cashew_set_bench.cpp -DBENCH_CASHEW && ./a.out
-clang++ -O3 --std=c++11 cashew_set_bench.cpp -DBENCH_GNU_MT_ALLOC && ./a.out
-clang++ -O3 --std=c++11 cashew_set_bench.cpp -DBENCH_STD  && ./a.out
+g++ -O3 --std=c++11 cashew_set_bench.cpp -DBENCH_CASHEW && ./a.out
+g++ -O3 --std=c++11 cashew_set_bench.cpp -DBENCH_GNU_MT_ALLOC && ./a.out
+g++ -O3 --std=c++11 cashew_set_bench.cpp -DBENCH_STD  && ./a.out
